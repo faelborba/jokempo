f5af259722e74f2adb52660ca2964e7d99ab94dd
unknown <rafael.bds25@gmail.com>
2020-03-15 18:38:37 -0300

    Inserindo resultado na aplicação

--- app/src/main/java/com/example/jokempo/MainActivity.java
+++ app/src/main/java/com/example/jokempo/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     ImageButton botaoPedra, botaoPapel, botaoTesoura;
 
     Animation some, aparece;
+
+    int jogada1=0;
+    int jogada2=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +56,14 @@ public class MainActivity extends AppCompatActivity {
         aparece.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
+                sorteiaJogadaInimigo();
                 jogador2.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
                 jogador2.setVisibility(View.VISIBLE);
+                verificaJogada();
             }
 
             @Override
@@ -70,11 +78,47 @@ public class MainActivity extends AppCompatActivity {
     public void tocouBotao(View view){
         jogador1.setScaleX(-1);
         switch (view.getId()){
-            case(R.id.botaoPedra):jogador1.setImageResource(R.drawable.pedra); break;
-            case(R.id.botaoPapel):jogador1.setImageResource(R.drawable.papel);break;
-            case(R.id.botaoTesoura):jogador1.setImageResource(R.drawable.tesoura); break;
+            case(R.id.botaoPedra):
+                jogador1.setImageResource(R.drawable.pedra);
+                jogada1 = 1;
+                break;
+            case(R.id.botaoPapel):
+                jogador1.setImageResource(R.drawable.papel);
+                jogada1 = 2;
+                break;
+            case(R.id.botaoTesoura):
+                jogador1.setImageResource(R.drawable.tesoura);
+                jogada1 = 3;
+                break;
         }
         jogador2.setImageResource(R.drawable.interrogacao);
         jogador2.startAnimation(some);
+    }
+
+    public void sorteiaJogadaInimigo(){
+        Random r = new Random();
+        int sort = r.nextInt(3) + 1;
+        if (sort == 1){
+            jogador2.setImageResource(R.drawable.pedra);
+        }else if ( sort == 2){
+            jogador2.setImageResource(R.drawable.papel);
+        }else if (sort == 3){
+            jogador2.setImageResource(R.drawable.tesoura);
+        }
+        jogada2 = sort;
+    }
+    public void verificaJogada(){
+        if (jogada1 == jogada2){
+            Toast.makeText(this,"Empate!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if ((jogada1==1 && jogada2 == 3)||(jogada1==2 && jogada2 == 1)||(jogada1==3 && jogada2 == 2)){
+            Toast.makeText( this,"Você venceu!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if ((jogada1==1 && jogada2 == 2)||(jogada1==2 && jogada2 == 3)||(jogada1==3 && jogada2 == 1)){
+            Toast.makeText( this,"Você perdeu!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
